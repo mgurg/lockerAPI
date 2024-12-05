7b233a239bd59512d51abe69f41942ef1e78d8a0
mgurg <34517378+mgurg@users.noreply.github.com>
2024-12-05 15:36:12 +0100

    Basic endpoint protection

--- app/controller/games.py
+++ app/controller/games.py
@@ -4,6 +4,7 @@ from uuid import UUID
 from fastapi import APIRouter, Depends
 from starlette.status import HTTP_204_NO_CONTENT
 
+from app.datbase.bearer_auth import is_app_owner
 from app.schemas.ai_game import (
     AnswerRequest,
     AnswerResponse,
@@ -22,31 +23,31 @@ gameServiceDependency = Annotated[GameService, Depends()]
 
 
 @game_router.post("/start")
-async def start_game(game_service: gameServiceDependency, setup: GameStart) -> GameStartResponse:
+async def start_game(game_service: gameServiceDependency, setup: GameStart, auth=Depends(is_app_owner)) -> GameStartResponse:
     return await game_service.start(setup)
 
 
 @game_router.get("/intro/{game_uuid}")
-async def get_intro(game_service: gameServiceDependency, game_uuid: UUID) -> IntroResponse:
+async def get_intro(game_service: gameServiceDependency, game_uuid: UUID, auth=Depends(is_app_owner)) -> IntroResponse:
     return await game_service.intro(game_uuid)
 
 
 @game_router.get("/puzzle/{game_uuid}")
-async def get_puzzle(game_service: gameServiceDependency, game_uuid: UUID) -> CurrentPuzzleResponse:
+async def get_puzzle(game_service: gameServiceDependency, game_uuid: UUID, auth=Depends(is_app_owner)) -> CurrentPuzzleResponse:
     return await game_service.generate_puzzle(game_uuid)
 
 
 @game_router.post("/answer/{game_uuid}")
-async def submit_answer(game_service: gameServiceDependency, game_uuid: UUID, answer: AnswerRequest) -> AnswerResponse:
+async def submit_answer(game_service: gameServiceDependency, game_uuid: UUID, answer: AnswerRequest, auth=Depends(is_app_owner)) -> AnswerResponse:
     return await game_service.answer(game_uuid, answer.choice)
 
 
 @game_router.get("/ending/{game_uuid}")
-async def get_ending(game_service: gameServiceDependency, game_uuid: UUID) -> GameOutro:
+async def get_ending(game_service: gameServiceDependency, game_uuid: UUID, auth=Depends(is_app_owner)) -> GameOutro:
     return await game_service.ending(game_uuid)
 
 
 @game_router.post("/review/{game_uuid}", status_code=HTTP_204_NO_CONTENT)
-async def add_review(game_service: gameServiceDependency, game_uuid: UUID, review: ReviewRequest) -> None:
+async def add_review(game_service: gameServiceDependency, game_uuid: UUID, review: ReviewRequest, auth=Depends(is_app_owner)) -> None:
     await game_service.review(game_uuid, review)
     return None
---
+++ app/datbase/bearer_auth.py
@@ -0,0 +1,32 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBasicCredentials, HTTPBearer
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+from app.config import get_settings
+
+settings = get_settings()
+security = HTTPBearer()
+
+
+def check_token(
+        credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+):
+    """
+    Function that is used to validate the token in the case that it requires it
+    """
+    token = credentials.credentials
+    if token is None:
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Missing auth token")
+
+    raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Incorrect auth token")
+
+
+def is_app_owner(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    token = credentials.credentials
+    if token is None:
+        raise HTTPException(status_code=401, detail="Missing auth token")
+
+    if len(token) != 31:
+        raise HTTPException(status_code=401, detail="Missing auth token")
